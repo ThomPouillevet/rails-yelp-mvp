06ec2c093652432a7c7c0fa4b65f29bf7cb1a77a
Thomas Pouillevet <thomas.pouillevet@gmail.com>
2018-08-15 13:10:09 +0200

    done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,22 +9,45 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    respond_to do |format|
+      if @restaurant.save
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully created.' }
+        format.json { render :show, status: :created, location: @restaurant }
+      else
+        format.html { render :new }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def show
     # ...
   end
+
   def edit
     # ...
   end
+
   def update
     # ...
   end
+
   def destroy
     # ...
   end
 
   private
 
+  # Use callbacks to share common setup or constraints between actions.
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,4 @@ class Review < ApplicationRecord
   validates :content, :rating, presence: true
   validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: { in: 0..5 }
-
 end
